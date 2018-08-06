5d4fdf9589fd3c4953f83932fc7683b897c6ebf7
AJW44 <bookkeepingajw@gmail.com>
2018-08-06 23:02:44 +1200

    Commit setColor and fillRect

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -4,40 +4,56 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
 	
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer (TIMER_DELAY, this);
+			timer.start();
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void keyReleased(KeyEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	private void update() {
+		
+	}
+	
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent event) {
+		update();
 		
 	}
 
 
-public static void main(String[] args) {
+public static void main(String[] event) {
 }
 }
